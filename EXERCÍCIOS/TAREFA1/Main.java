d6394b4730f114bf0901df01ca2f703ceeaed862
Yasmim Raposo <137338416+YasmimRap0S0@users.noreply.github.com>
2025-10-26 22:06:59 +0000

    Pilha, ok!

--- EXERCÍCIOS/TAREFA1/Main.java
+++ EXERCÍCIOS/TAREFA1/Main.java
@@ -2,28 +2,109 @@ package TAREFA1;
 
 public class Main {
     public static void main(String[] args) {
-        PilhaColorida pilha = new PilhaColorida(3);
+        PilhaColorida pilha = new PilhaColorida(6);
 
-        for (int i = 0; i < 3; i++) { //
-            pilha.pushVermelha(i);
-            pilha.pushPreta(i + 1);
-        }
+         /* pilha.pushVermelha(1);
+        pilha.pushVermelha(2);
+        pilha.pushVermelha(3);
+        pilha.pushVermelha(4);
+        pilha.pushPreta(5);
+        pilha.pushPreta(6);
+        pilha.pushPreta(7);
+        pilha.pushPreta(8);
+        pilha.pushPreta(9);
+        pilha.pushPreta(10);
+
+       */
+        pilha.pushVermelha(1);
+                pilha.pushVermelha(5);
+
+        pilha.pushPreta(2);
+
+        pilha.ver();
 
         System.out.println("Topo Vermelha: " + pilha.topVermelha());
+        System.out.println("TopPreta: " + pilha.topPreta());
+
+        pilha.ver();
+
+
+        System.out.println("Pop Vermelha: " + pilha.popVermelha());
+        
+
+        pilha.ver();
+
+        System.out.println("Pop preta: " + pilha.popPreta());
+
+        pilha.ver();
+        pilha.pushPreta(1);
+        pilha.ver();
+
+        pilha.pushPreta(2);
+        pilha.ver();
+
+        pilha.pushPreta(3);
+
+        pilha.ver();
+        System.out.println("Pop preta: " + pilha.popPreta());
+        System.out.println("Pop preta: " + pilha.popPreta());
+        pilha.ver();
+        System.out.println("Pop preta: " + pilha.popPreta());
+        pilha.ver();
+        pilha.pushPreta(1);
+        pilha.ver();
+        System.out.println("Pop preta: " + pilha.popPreta());
+        pilha.ver();
+
+        pilha.pushVermelha(1);
+        pilha.ver();
+        pilha.pushVermelha(2);
+        pilha.ver();
+        pilha.pushVermelha(1);
+        pilha.ver();
+        pilha.pushVermelha(2);
+        pilha.ver();
+
+
+
+
+
+        
+    /* 
+        System.out.println("testando método duplicação:");
+        
+        pilha.pushVermelha(1);
+        pilha.pushVermelha(2);
+        pilha.pushVermelha(3);
+        pilha.pushVermelha(4);
+        pilha.pushPreta(5);
+        pilha.pushPreta(6);
+        pilha.pushPreta(7);
+        pilha.pushPreta(8);
+        pilha.pushPreta(9);
+        pilha.pushPreta(10);
+         System.out.println("Topo Vermelha: " + pilha.topVermelha());
+        System.out.println("Topo Preta: " + pilha.topPreta());
+        pilha.ver();
+
+        
+        System.out.println("testando dnv:");
+       
+        pilha.pushPreta(15);
+        pilha.ver();
         System.out.println("Topo Preta: " + pilha.topPreta());
 
-        while (!pilha.isEmptyVermelha()) {
-            System.out.println("Pop Vermelha: " + pilha.popVermelha());
-        }
 
-        while (!pilha.isEmptyPreta()) {
-            System.out.println("Pop Preta: " + pilha.popPreta());
-        }
-        for (int i = 0; i < array.length; i++) {
-        System.out.print(array[i] + " ");
-    }
-    System.out.println("]");
-}
+*/
+/*
 
+pilha.pushVermelha(8);
+pilha.ver();
+pilha.pushVermelha(8);
+pilha.ver();
+pilha.pushVermelha(8);
+pilha.ver();
+    */
     }
 }
+
--- EXERCÍCIOS/TAREFA1/PilhaColorida.java
+++ EXERCÍCIOS/TAREFA1/PilhaColorida.java
@@ -24,12 +24,16 @@ class PilhaColorida {
     }
 
     public boolean isEmptyVermelha() {
-        if (topVermelha == -1) return true;
+        if (topVermelha == -1) {
+            return true;
+        }
         return false;
     }
 
     public boolean isEmptyPreta() {
-        if (topPreta == array.length) return true;
+        if (topPreta == array.length) {
+            return true;
+        }
         return false; //topPreta == size, significa que quando eu for inserir o primeiro elemento, ele estará na posicao size-1
     }
 
@@ -43,61 +47,90 @@ class PilhaColorida {
         return array[topPreta]; 
     }
 
-
-    //pop retira o elemento do topo e topo tá onde? mais a direta da pilha vermelha
-    public Object popVermelha() {
-        if (isEmptyVermelha()) throw new EPilhaVazia("Pilha vermelha vazia.");
-        Object elemento = array[topVermelha];
-        array[topVermelha--] = null; 
-        verificarReducao();
-        return elemento;
-    }
-
-    //faz o contrario, topPreta tá mais a esquerda da pilha preta
-    public Object popPreta() {
-        if (isEmptyPreta()) throw new EPilhaVazia("Pilha preta vazia.");
-        Object elemento = array[topPreta];
-        array[topPreta++] = null;
-        verificarReducao();
-        return elemento;
-    }
-
-    //e se o prox indice da pilha vermelha for o mesmo da preta?
-
     public void pushVermelha(Object x) {
-        if (topVermelha + 1 == topPreta) duplicar(array.length * 2);
-        array[++topVermelha] = x;
+        if (topVermelha + 1 == topPreta){
+            duplicar();
+        } 
+        topVermelha++;
+        array[topVermelha] = x;
     }
 
     public void pushPreta(Object x) {
-        if (topVermelha + 1 == topPreta) duplicar(array.length * 2);
-        array[--topPreta] = x; //apenas decrementamos já que insere da direita p/ esquerda
+        if (topPreta - 1 == topVermelha){
+            duplicar();
+        }
+        topPreta--;
+        array[topPreta] = x; 
     }
 
-    private void duplicar(int novoTam) {
-        Object[] aux = new Object[novoTam]; //cria array auxiliar
-        for (int i = 0; i <= topVermelha; i++) {
-            aux[i] = array[i]; //copia elem.vermelhos
-        }
-
-        //como realocar o indice do preto?Ex.: topPreta tá na posicao 8, array.length é 10, novoTam é 20
-        //topPreta deve ir para a posicao 18... ent, eu descubro quantos elementos tem na pilha preta velha e realoco pra definir topPreta nova
+    public Object popVermelha() {
+        if (isEmptyVermelha()) throw new EPilhaVazia("Pilha vermelha vazia.");
+        reduzir();
+        Object elemento = array[topVermelha];
+        array[topVermelha] = null;
+        topVermelha--;
+        return elemento;
         
-        int topPretanova = novoTam - (array.length - topPreta); 
-        for (int i = topPreta; i < array.length; i++) {
-            aux[topPretanova + i - topPreta] = array[i];
+    }
+
+    public Object popPreta() {
+        if (isEmptyPreta()) throw new EPilhaVazia("Pilha preta vazia.");
+        reduzir();
+        Object elemento = array[topPreta];
+        array[topPreta] = null;
+        topPreta++;
+        return elemento;
+        
+    }
+
+    private void duplicar() {
+        Object[] aux = new Object[array.length*2];
+        for(int i = 0; i <= topVermelha; i++){
+            aux[i] = array[i]; //ele.vermelho
+        };
+        int qtdPreta = sizePreta();
+        
+        int topPretanova = aux.length - qtdPreta;
+        for (int i = topPretanova; i < aux.length; i++) {
+            aux[i] = array[topPreta];
+            topPreta++;
+     }
+    topPreta = topPretanova;
+    array = aux;
+};
+
+    // se tenho qtd X de elem na array com capacidade pra 10 e ela tem 1/3 ou 1/4 apenas de utilizaçõa, reduz
+
+    private void reduzir() {
+        int qtdElem = sizePreta() + sizeVermelha();
+        if  ((float) qtdElem / array.length <= (float) 1/3) {
+
+         
+            Object[] aux = new Object[array.length/2];
+            for(int i = 0; i <= topVermelha; i++){
+                aux[i] = array[i]; //ele.vermelho
+            };
+            int qtdPreta = sizePreta();
+            
+            int topPretanova = aux.length - qtdPreta;
+            for (int i = topPretanova; i < aux.length; i++) {
+                aux[i] = array[topPreta];
+                topPreta++;
         }
         topPreta = topPretanova;
-        array = aux; //copiar toda a array auxiliar para a original
+        array = aux;
     }
+};
 
-    //pra reduzir... 1/3 do array em uso
-    //e se minha array for de tamanho 1? ai n da pra reduzir a array mais, ent... apenas reduzo se for maior que 1
-
-    private void verificarReducao() {
-        int totalArray = (topVermelha + 1) + (array.length - topPreta);
-        if (totalArray <= array.length / 3 && array.length > 1) duplicar(array.length / 2); //Dá pra reutilizar o método duplicar pq ele faz a realocação dos elementos
+    public void ver() {
+    System.out.print("Array atual:");
+    for (int i = 0; i < array.length; i++) {
+        System.out.print(array[i] + " ");
     }
+    System.out.println();
+}
+
+
 
 class EPilhaVazia extends RuntimeException {
     public EPilhaVazia(String err) {
